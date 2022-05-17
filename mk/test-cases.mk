2f6f48c02a608f8dcc48a5a19c20c3011fce3a24
Gabriel Ferreira <gabrielcarvfer@gmail.com>
2022-05-17 16:50:38 -0300

    Trying to add more applications

--- mk/test-cases.mk
+++ mk/test-cases.mk
@@ -47,6 +47,48 @@ test-rsm-dataplane: $(M)/ric $(M)/omec $(M)/oai-ue
 	iperf3 -s -B $$(ip a show oaitun_ue1 | grep inet | grep -v inet6 | awk '{print $$2}' | awk -F '/' '{print $$1}') -p 5001 > /dev/null &
 	kubectl exec -it router -- iperf3 -u -c $$(ip a show oaitun_ue1 | grep inet | grep -v inet6 | awk '{print $$2}' | awk -F '/' '{print $$1}') -p 5001 -b 20M -l 1450 -O 2 -t 12 --get-server-output
 	pkill -9 -ef iperf3
+	@echo "*** Test downlink traffic (TCP) ***"
+	iperf3 -s -B $$(ip a show oaitun_ue1 | grep inet | grep -v inet6 | awk '{print $$2}' | awk -F '/' '{print $$1}') -p 5001 > /dev/null &
+	kubectl exec -it router -- iperf3 -c $$(ip a show oaitun_ue1 | grep inet | grep -v inet6 | awk '{print $$2}' | awk -F '/' '{print $$1}') -p 5001 -b 20M -l 1450 -O 2 -t 12 --get-server-output
+	pkill -9 -ef iperf3
+
+test-bigbuckbunny-dataplane: $(M)/ric $(M)/omec $(M)/oai-ue
+	@echo "*** Test downlink traffic (UDP) ***"
+	sudo apt install -y vlc tshark
+	kubectl exec -it router -- apt install -y vlc
+	sudo tshark -w output.pcap -a duration:15 &
+	sleep 1
+	cvlc ./BigBuckBunny.mp4 --loop --sout '#transcode{vcodec=h264,acodec=mpga,vb=125k,ab=64k,deinterlace,scale=0.25,threads=2}:http{mux=ts,dst=172.250.255.254:5001}' &
+	#kubectl exec -it router -- userdel vlc
+	#kubectl exec -it router -- useradd -m vlc
+	#kubectl exec -it router -- passwd vlc -p 1
+	kubectl exec -it router -- vlc http://172.250.255.254:5001 --sout="#duplicate{dst=std{access=file,mux=avi,dst=stream.avi},dst=nodisplay}" &
+	sleep 10
+	kubectl exec -it router -- pkill -9 -ef vlc
+	pkill -9 -ef cvlc
+
+#test-flent-dataplane: $(M)/ric $(M)/omec $(M)/oai-ue
+#	@echo "*** Test downlink traffic (UDP) ***"
+#	echo "deb [trusted=yes] https://ppa.launchpadcontent.net/deadsnakes/ppa/ubuntu bionic main" | sudo tee -a /etc/apt/sources.list
+#	sudo apt update
+#	sudo apt install -y netperf python3.5 curl
+#	sudo cp --force /usr/bin/python3.5 /usr/bin/python3
+#	curl https://bootstrap.pypa.io/pip/3.5/get-pip.py -o get-pip.py
+#	python3 get-pip.py
+#	sudo pip install matplotlib flent
+#	kubectl exec -it router -- echo "deb [trusted=yes] https://ppa.launchpadcontent.net/deadsnakes/ppa/ubuntu bionic main" | sudo tee -a /etc/apt/sources.list
+#	kubectl exec -it router -- apt update
+#	kubectl exec -it router -- apt install -y netperf python3.5 curl
+#	kubectl exec -it router -- cp --force /usr/bin/python3.5 /usr/bin/python3
+#	kubectl exec -it router -- curl https://bootstrap.pypa.io/pip/3.5/get-pip.py -o get-pip.py
+#	kubectl exec -it router -- python3 get-pip.py
+#	kubectl exec -it router -- pip install matplotlib flent
+#	flent --local-bind $$(ip a show oaitun_ue1 | grep inet | grep -v inet6 | awk '{print $$2}' | awk -F '/' '{print $$1}') host &
+#	kubectl exec -it router -- flent rrul -p all_scaled -t "RRUL all" -l 60 -H $$(ip a show oaitun_ue1 | grep inet | grep -v inet6 | awk '{print $$2}' | awk -F '/' '{print $$1}')
+#	kubectl exec -it router -- flent rrul -p ping_cdf -t "RRUL ping cdf" -l 60 -H $$(ip a show oaitun_ue1 | grep inet | grep -v inet6 | awk '{print $$2}' | awk -F '/' '{print $$1}')
+#	kubectl exec -it router -- flent tcp_upload -t "TCP UP" -p totals -l 60 -H $$(ip a show oaitun_ue1 | grep inet | grep -v inet6 | awk '{print $$2}' | awk -F '/' '{print $$1}')
+#	kubectl exec -it router -- flent tcp_download -t "TCP DOWN" -p totals -l 60 -H $$(ip a show oaitun_ue1 | grep inet | grep -v inet6 | awk '{print $$2}' | awk -F '/' '{print $$1}')
+#	pkill -9 -ef flent
 
 test-mho: | $(M)/ric
 	@echo "*** Get MHO result through CLI - Cells ***"; \
